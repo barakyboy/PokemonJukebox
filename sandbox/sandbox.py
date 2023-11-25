bd7205acafe521c1d5085ee8c8ae393d6af44773
barakyBoy <uribaroque@gmail.com>
2023-11-25 19:58:40 +0200

    added requirements that make model run?

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,13 +1,13 @@
 # from pyboy import PyBoy
 # from pyboy import WindowEvent
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 # import os
 # from pytube import YouTube
 from basic_pitch.inference import predict
 # from basic_pitch import ICASSP_2022_MODEL_PATH
 
 
-# load_dotenv()
+load_dotenv()
 # GAME_PATH = os.getenv('GAME_PATH')
 #
 # yt = YouTube('https://www.youtube.com/watch?v=SYTS2sJWcIs&ab_channel=Pokeli')
