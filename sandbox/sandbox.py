64068cb887dc68f582f1c141750d9588703b73b9
barakyBoy <uribaroque@gmail.com>
2023-11-25 09:39:40 +0200

    wrote more of sandbox script

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,9 +1,32 @@
 from pyboy import PyBoy
 from pyboy import WindowEvent
+from dotenv import load_dotenv
+import os
+from pytube import YouTube
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
 
-ROM_PATH = 'assets/game.gb'
+
+load_dotenv()
+GAME_PATH = os.getenv('GAME_PATH')
+
+yt = YouTube('https://www.youtube.com/watch?v=SYTS2sJWcIs&ab_channel=Pokeli')
+video = yt.streams.filter(only_audio=True).first()
+
+# set up path
+current_directory = os.getcwd()
+
+# Construct the path one level up and then into the "assets" folder
+music_path = os.path.join(current_directory, "..", "assets")
+
+
+out_file = video.download(output_path=music_path)
+
+
 
 # instantiate game
-with PyBoy('assets/game.gb') as pyboy:
-    while not pyboy.tick():
-        pass
+# with PyBoy(GAME_PATH) as pyboy:
+#     while not pyboy.tick():
+#         pass
+
+# model_output, midi_data, note_events = predict()
