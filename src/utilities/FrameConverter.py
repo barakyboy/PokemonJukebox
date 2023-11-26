3d28a9cd87ccbc0c69109c87cf82f83680ae1e36
barakyBoy <uribaroque@gmail.com>
2023-11-26 12:38:01 +0200

    implemented midi pitch to pitch class conversion

--- src/utilities/FrameConverter.py
+++ src/utilities/FrameConverter.py
@@ -1,22 +1,26 @@
 from pretty_midi.pretty_midi import Note
 from math import floor
+from Pitch import Pitch
+
 
 class FrameConverter:
     """
-    A class that is responsible for converting PrettyMidi da
+    A class that is responsible for converting PrettyMidi data to frames and absolute pitch class
     """
 
     FPS = 60
+    NOTES_IN_OCTAVE = 12
 
     def convert_notes_to_frames(self, notes: list[Note]) -> list(tuple):
         """
         Takes an ordered (by onset) list of prettymidi notes and returns a list of 2-tuples, with
-        each tuple representing the frame representing the note onset (starting from 0), and the pitch
-        of the note in midi format (i.e, of the form (frame, pitch))
+        each tuple representing the frame representing the note onset (starting from 0), and the pitch class
+        of the note(i.e, of the form (frame, pitch))
         :param notes: a list of ordered (by onset) pretty_midi notes
-        :return: a list of 2-tuples, with
-        each tuple representing the frame representing the note onset (starting from 0), and the pitch
+        :return:  a list of 2-tuples, with
+        each tuple representing the frame representing the note onset (starting from 0), and the pitch class
+        of the note(i.e, of the form (frame, pitch))
         """
 
-        return [(floor(note.start * self.FPS), note.pitch) for note in notes]
+        return [(floor(note.start * self.FPS), Pitch(note.pitch % self.NOTES_IN_OCTAVE)) for note in notes]
 
