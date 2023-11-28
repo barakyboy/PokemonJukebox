f87173f520bb4ad6653c4632485a2cf6a6c1679c
barakyBoy <uribaroque@gmail.com>
2023-11-29 01:10:40 +0200

    wrote most of application steel thread in GUI_sandbox

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -4,15 +4,12 @@ from dotenv import load_dotenv
 import os
 import time
 from src.utilities.PitchControl import PitchControl
-import threading
 from src.utilities.Screen import Screen
 from src.utilities.FrameConverter import FrameConverter
 from queue import Queue
-from src.pipelines.add_song_to_queue import add_song_to_queue
-from pydub import AudioSegment
-from pydub.playback import play
 
-def main():
+
+def main(q: Queue):
 
     load_dotenv()
     GAME_PATH = os.getenv('GAME_PATH')
@@ -28,21 +25,11 @@ def main():
     release_dict[WindowEvent.PRESS_BUTTON_A] = WindowEvent.RELEASE_BUTTON_A
     release_dict[WindowEvent.PRESS_BUTTON_B] = WindowEvent.RELEASE_BUTTON_B
 
-    # song queue
-    queue = Queue()
-    LINK = 'https://www.youtube.com/watch?v=iWMN91gMD_4&ab_channel=JamesAlbert'
 
-    # add initial song to queue
-    add_song_to_queue(queue, link=LINK)
 
-    framed_notes = queue.get()
-
-    # get current note data
-    curr = framed_notes.pop(0)
-
-    # prepare music thread
-    audio = AudioSegment.from_file("C:\\Users\\uriba\\Desktop\\musicmon\\assets\\MUS\\mus7.mp3", format="mp4")
-    t = threading.Thread(target=play, args=(audio,))
+    # initial framed notes and curr states
+    framed_notes = []
+    curr = None
 
     # instantiate the gameboy screen visual
     screen = Screen()
@@ -51,54 +38,32 @@ def main():
     with PyBoy(GAME_PATH) as pyboy:
 
         # initial wait for pyboy graphic to end
-        time.sleep(15)
+        time.sleep(10)
 
-        frame_num = 0
+        # initialise pitch controller
         pitch_controller = PitchControl()
-
-        # deal with frame 0
-        if curr[0] == frame_num:
-
-            # there is an event on this frame, execute it
-            event = pitch_controller.get_control(curr[1])
-
-            # start music
-            t.start()
-
-            for i in range(FrameConverter.HOLD_FRAMES):
-                # hold for required number of frames
-                pyboy.send_input(event)
-
-                # update visual
-                screen.update(event)
-                # go to next frame
-                frame_num += 1
-                pyboy.tick()
-
-            # release input
-            pyboy.send_input(release_dict[event])
-
-            # hold visual of last input for another frame
-            screen.update(event)
-
-            # get next input
-            if len(framed_notes) == 0:
-                pyboy.tick()
-                exit()  # end program
-
-            curr = framed_notes.pop(0)
-
-        else:
-            # update visual
-            screen.update()
-
-            # start music
-            t.start()
-
-        frame_num += 1
 
         # game loop
         while not pyboy.tick():
+            # check if no more instructions
+            if curr is None:
+                # get more instructions
+                while q.empty():
+                    # yield
+                    time.sleep(0)
+
+                # queue is no longer empty, extract next song
+                framed_notes, t = q.get()
+
+                # get next note data
+                curr = framed_notes.pop(0)
+
+                # set frame to 0
+                frame_num = 0
+
+                # start song
+                t.start()
+
 
             # check if there is an event on this frame
             if curr[0] == frame_num:
@@ -122,12 +87,10 @@ def main():
                 # hold visual of last input for another frame
                 screen.update(event)
 
-                # get next input
                 if len(framed_notes) == 0:
-                    pyboy.tick()
-                    exit()  # end program
-
-                curr = framed_notes.pop(0)
+                    curr = None
+                else:
+                    curr = framed_notes.pop(0)
 
             else:
                 screen.update()
--- src/pipelines/add_song_to_queue.py
+++ src/pipelines/add_song_to_queue.py
@@ -5,12 +5,17 @@ from src.utilities.exceptions import NoInstrumentsFoundError, NoNotesFoundError
 from src.utilities.NoteFilterStrategy import TopNVelocityStrategy
 from src.utilities.FrameConverter import FrameConverter
 from queue import Queue
+import threading
+from pydub import AudioSegment
+from pydub.playback import play
+import os
 
 
 def add_song_to_queue(q: Queue, link: str):
     """
     Takes a queue and a youtube link as input and adds a list of framed notes corresponding to the processed link
-    to the queue
+    to the queue, along with a thread object for playing the song. So adds a tuple to the queue of the form:
+    (list of framed notes, tuple for playing song)
     :param q: a queue of lists of framed notes
     :param link: a youtube link
     """
@@ -37,5 +42,12 @@ def add_song_to_queue(q: Queue, link: str):
     # convert filtered notes to frames and pitch classes
     framed_notes = FrameConverter().convert_notes_to_frames(filtered_notes)
 
+    # create the thread object containing the song for playing
+    audio = AudioSegment.from_file(mp3_abs_path, format="mp4")
+    t = threading.Thread(target=play, args=(audio,))
+
+    # delete audio file
+    os.remove(mp3_abs_path)
+
     # add to queue
-    q.put(framed_notes)
+    q.put((framed_notes, t))
