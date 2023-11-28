4876008e9c11d015404cc9269ba6d01da1a39c63
barakyBoy <uribaroque@gmail.com>
2023-11-28 22:33:25 +0200

    tinkered with UI setup

--- sandbox/GUI_sandbox.py
+++ sandbox/GUI_sandbox.py
@@ -1,13 +1,23 @@
-root = tk.Tk()
-root.title("Link Processor")
+import tkinter as tk
+from sandbox import main
+from threading import Thread
 
-# Create a text box for entering links
-entry = tk.Text(root, height=5, width=50)
-entry.pack(pady=10)
+
+def start_game():
+    start_game_button.pack_forget()
+    game.start()
+
+
+root = tk.Tk()
+root.title("Game")
+
+# create game thread
+game = Thread(target=main)
 
 # Create a button to trigger link processing
-process_button = tk.Button(root, text="Process Links", command=process_links)
-process_button.pack()
+start_game_button = tk.Button(root, text="Start Game", command=start_game)
+start_game_button.pack()
+
 
 # Start the Tkinter event loop
 root.mainloop()
--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -12,98 +12,58 @@ from src.pipelines.add_song_to_queue import add_song_to_queue
 from pydub import AudioSegment
 from pydub.playback import play
 
+def main():
 
-load_dotenv()
-GAME_PATH = os.getenv('GAME_PATH')
+    load_dotenv()
+    GAME_PATH = os.getenv('GAME_PATH')
 
-# initialise controls dictionary
-release_dict = dict()
-release_dict[WindowEvent.PRESS_ARROW_UP] = WindowEvent.RELEASE_ARROW_UP
-release_dict[WindowEvent.PRESS_ARROW_DOWN] = WindowEvent.RELEASE_ARROW_DOWN
-release_dict[WindowEvent.PRESS_ARROW_LEFT] = WindowEvent.RELEASE_ARROW_LEFT
-release_dict[WindowEvent.PRESS_ARROW_RIGHT] = WindowEvent.RELEASE_ARROW_RIGHT
-release_dict[WindowEvent.PRESS_BUTTON_START] = WindowEvent.RELEASE_BUTTON_START
-release_dict[WindowEvent.PRESS_BUTTON_SELECT] = WindowEvent.RELEASE_BUTTON_SELECT
-release_dict[WindowEvent.PRESS_BUTTON_A] = WindowEvent.RELEASE_BUTTON_A
-release_dict[WindowEvent.PRESS_BUTTON_B] = WindowEvent.RELEASE_BUTTON_B
+    # initialise controls dictionary
+    release_dict = dict()
+    release_dict[WindowEvent.PRESS_ARROW_UP] = WindowEvent.RELEASE_ARROW_UP
+    release_dict[WindowEvent.PRESS_ARROW_DOWN] = WindowEvent.RELEASE_ARROW_DOWN
+    release_dict[WindowEvent.PRESS_ARROW_LEFT] = WindowEvent.RELEASE_ARROW_LEFT
+    release_dict[WindowEvent.PRESS_ARROW_RIGHT] = WindowEvent.RELEASE_ARROW_RIGHT
+    release_dict[WindowEvent.PRESS_BUTTON_START] = WindowEvent.RELEASE_BUTTON_START
+    release_dict[WindowEvent.PRESS_BUTTON_SELECT] = WindowEvent.RELEASE_BUTTON_SELECT
+    release_dict[WindowEvent.PRESS_BUTTON_A] = WindowEvent.RELEASE_BUTTON_A
+    release_dict[WindowEvent.PRESS_BUTTON_B] = WindowEvent.RELEASE_BUTTON_B
 
-# song queue
-queue = Queue()
-LINK = 'https://www.youtube.com/watch?v=Xqf8jID9TsE&ab_channel=TerraBlue'
+    # song queue
+    queue = Queue()
+    LINK = 'https://www.youtube.com/watch?v=iWMN91gMD_4&ab_channel=JamesAlbert'
 
-# add initial song to queue
-add_song_to_queue(queue, link=LINK)
+    # add initial song to queue
+    add_song_to_queue(queue, link=LINK)
 
-framed_notes = queue.get()
+    framed_notes = queue.get()
 
-# get current note data
-curr = framed_notes.pop(0)
+    # get current note data
+    curr = framed_notes.pop(0)
 
-# prepare music thread
-audio = AudioSegment.from_file("C:\\Users\\uriba\\Desktop\\musicmon\\assets\\MUS\\mus1.mp3", format="mp4")
-t = threading.Thread(target=play, args=(audio,))
+    # prepare music thread
+    audio = AudioSegment.from_file("C:\\Users\\uriba\\Desktop\\musicmon\\assets\\MUS\\mus7.mp3", format="mp4")
+    t = threading.Thread(target=play, args=(audio,))
 
-# instantiate the gameboy screen visual
-screen = Screen()
+    # instantiate the gameboy screen visual
+    screen = Screen()
 
-# instantiate game
-with PyBoy(GAME_PATH) as pyboy:
+    # instantiate game
+    with PyBoy(GAME_PATH) as pyboy:
 
-    # initial wait for pyboy graphic to end
-    time.sleep(15)
+        # initial wait for pyboy graphic to end
+        time.sleep(15)
 
-    frame_num = 0
-    pitch_controller = PitchControl()
+        frame_num = 0
+        pitch_controller = PitchControl()
 
-    # deal with frame 0
-    if curr[0] == frame_num:
-
-        # there is an event on this frame, execute it
-        event = pitch_controller.get_control(curr[1])
-
-        # start music
-        t.start()
-
-        for i in range(FrameConverter.HOLD_FRAMES):
-            # hold for required number of frames
-            pyboy.send_input(event)
-
-            # update visual
-            screen.update(event)
-            # go to next frame
-            frame_num += 1
-            pyboy.tick()
-
-        # release input
-        pyboy.send_input(release_dict[event])
-
-        # hold visual of last input for another frame
-        screen.update(event)
-
-        # get next input
-        if len(framed_notes) == 0:
-            pyboy.tick()
-            exit()  # end program
-
-        curr = framed_notes.pop(0)
-
-    else:
-        # update visual
-        screen.update()
-
-        # start music
-        t.start()
-
-    frame_num += 1
-
-    # game loop
-    while not pyboy.tick():
-
-        # check if there is an event on this frame
+        # deal with frame 0
         if curr[0] == frame_num:
 
             # there is an event on this frame, execute it
             event = pitch_controller.get_control(curr[1])
+
+            # start music
+            t.start()
 
             for i in range(FrameConverter.HOLD_FRAMES):
                 # hold for required number of frames
@@ -129,10 +89,51 @@ with PyBoy(GAME_PATH) as pyboy:
             curr = framed_notes.pop(0)
 
         else:
+            # update visual
             screen.update()
 
-        # increment frame num
+            # start music
+            t.start()
+
         frame_num += 1
+
+        # game loop
+        while not pyboy.tick():
+
+            # check if there is an event on this frame
+            if curr[0] == frame_num:
+
+                # there is an event on this frame, execute it
+                event = pitch_controller.get_control(curr[1])
+
+                for i in range(FrameConverter.HOLD_FRAMES):
+                    # hold for required number of frames
+                    pyboy.send_input(event)
+
+                    # update visual
+                    screen.update(event)
+                    # go to next frame
+                    frame_num += 1
+                    pyboy.tick()
+
+                # release input
+                pyboy.send_input(release_dict[event])
+
+                # hold visual of last input for another frame
+                screen.update(event)
+
+                # get next input
+                if len(framed_notes) == 0:
+                    pyboy.tick()
+                    exit()  # end program
+
+                curr = framed_notes.pop(0)
+
+            else:
+                screen.update()
+
+            # increment frame num
+            frame_num += 1
 
 
 
