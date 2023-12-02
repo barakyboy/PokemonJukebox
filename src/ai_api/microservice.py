8285bce720a59d23f069c5ba7f9938ab66780719
barakyBoy <uribaroque@gmail.com>
2023-12-02 18:37:13 +0200

    added binding to port environment variable

--- src/ai_api/microservice.py
+++ src/ai_api/microservice.py
@@ -1,5 +1,10 @@
 # a flask microservice used to run basic-pitch AI over an mp3 file
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+PORT = os.getenv('PORT')
 
 app = Flask(__name__)
 
@@ -7,3 +12,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(port=PORT)
