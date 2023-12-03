6eefd829c370de3f2d3aff93ade4c74388c0504b
barakyBoy <uribaroque@gmail.com>
2023-12-03 21:14:46 +0200

    wrote AI processing part of microservice

--- src/ai_api/microservice.py
+++ src/ai_api/microservice.py
@@ -7,6 +7,8 @@ from src.utilities.MusicDownloader import MusicDownloader
 from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
 import tensorflow as tf
+import threading
+
 
 load_dotenv()
 
@@ -14,6 +16,7 @@ basic_pitch_model = tf.saved_model.load(str(ICASSP_2022_MODEL_PATH))
 PORT = os.getenv('PORT')
 
 app = Flask(__name__)
+app.config['MP3_DIR'] = os.getenv('MP3_DIR')
 app.config['SECRET_KEY'] = os.getenv('PYTHONANYWHERE_API_TOKEN')
 
 
@@ -41,17 +44,29 @@ def key_required(f):
 
 @app.route("/process_mp3", methods=['POST'])
 @key_required
-def process_link():
-    return jsonify({'message': 'successfully authorized!'})
-    data = request.get_json()
-    link = data.get('link')
+def process_mp3():
 
-    # initialise mp3 abs path
-    mp3_abs_path = ''
+    if 'file' not in request.files:
+        return jsonify({'error': 'no file key provided'}), 400
+
+    file = request.files['file']
+
+    # acquire lock for determining filename
+    mutex = threading.Lock()
+
+    # get a file name and save the file
+    with mutex:
+        i = 0
+        candidate_path = os.path.join(app.config['MP3_DIR'], str(i)) + ".mp3"
+        while os.path.isfile(candidate_path):
+            i += 1
+            candidate_path = os.path.join(app.config['MP3_DIR'], str(i)) + ".mp3"
+
+        # save file to path
+        mp3_abs_path = candidate_path
+        file.save(mp3_abs_path)
 
     try:
-        downloader = MusicDownloader()
-        mp3_abs_path = downloader.download_youtube_link(link)
 
         # run AI over video
         midi_data = predict(mp3_abs_path)[1]
@@ -59,6 +74,7 @@ def process_link():
 
     except Exception as e:
         return jsonify(str(e)), 500
+
     finally:
         if os.path.isfile(mp3_abs_path):
             os.remove(mp3_abs_path)
