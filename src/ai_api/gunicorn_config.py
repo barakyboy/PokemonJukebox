cd8be96cfe05366afd67082e0faff3b745a95fdb
barakyBoy <uribaroque@gmail.com>
2023-12-17 15:29:29 +0200

    added gunicorn timeout

--- src/ai_api/gunicorn_config.py
+++ src/ai_api/gunicorn_config.py
@@ -6,7 +6,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '10'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
